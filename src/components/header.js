b030c3a4fb03e63d173a32bf2962a2e6a5fe68ed
Sayan <sayantanguharay@gmail.com>
2020-10-20 03:20:04 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -77,8 +77,8 @@ const Header = ({ siteTitle }) => (
     <Content>
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">
+        <NavLink to="/about">About</NavLink>
+        <GitHubLink href="https://github.com/SayanUX">
           GitHub
         </GitHubLink>
       </p>
